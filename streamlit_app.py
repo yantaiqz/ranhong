8d6fd91d4c09059f5321476205c4849c41939782
yantaiqz <yantaiqz@googlemail.com>
2025-12-11 09:25:21 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -262,7 +262,7 @@ def create_graph(data_frame, max_mc, max_shareholder_value):
             title=tooltip,
             group='国资股东',
             color={
-                'background': '#FFFFFF' #red_color,  # 统一红色
+                'background': '#FFFFFF' #red_color 统一红色
                 'border': '#FFFFFF'      # 边框白色
                 'highlight': {'background': '#FF5252', 'border': '#FFFFFF'}
             },
